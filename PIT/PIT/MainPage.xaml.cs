c3342fe684b1fcd702afc3bd99875e588966e987
jonas pontes <pontesjonas060@gmail.com>
2022-09-22 20:13:43 -0300

    Initial Commit

---
+++ PIT/PIT/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PIT
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void webviewNavigating(object sender, WebNavigatingEventArgs e)
+        {
+            pagina.IsVisible = false;
+            logo.IsVisible = true;
+            labelLoading.IsEnabled = true;
+            labelLoading.IsRefreshing = true;
+        }
+
+        void webviewNavigated(object sender, WebNavigatedEventArgs e)
+        {
+            pagina.IsVisible = true;
+            logo.IsVisible = false;
+            labelLoading.IsRefreshing = false;
+            labelLoading.IsEnabled = false;
+
+        }
+    }
+}
